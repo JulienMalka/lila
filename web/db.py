11164576cf6fcd14fa81fc47de694adbad6720b1
Julien Malka <julien@malka.sh>
2024-02-24 23:30:53 +0100

    clean

--- web/db.py
+++ web/db.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./hashes.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}

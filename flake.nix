e437b59f264f8473b597a7975f5bef359f697d49
Julien Malka <julien@malka.sh>
2024-02-24 20:05:21 +0100

    update supported systems

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
 
-    (flake-utils.lib.eachDefaultSystem (system:
+    (flake-utils.lib.eachSystem [ system.x86_64-linux system.aarch64-linux ] (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
         packages = rec {

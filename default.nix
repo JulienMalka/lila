eaa6511e804e93198320768547089b8020f742f0
Julien Malka <julien@malka.sh>
2024-02-24 18:52:36 +0100

    init flake compat

--- default.nix
+++ default.nix
@@ -1,31 +1,10 @@
-{
-  sources ? import ./npins,
-  pkgs ? import sources.nixpkgs { },
-}:
-
-{
-  shell = pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
-    buildInputs = [
-      (pkgs.python3.withPackages (
-        ps: [
-          ps.fastapi
-          ps.uvicorn
-          ps.sqlalchemy
-          ps.pydantic
-        ]
-      ))
-      pkgs.jq
-      pkgs.rust-analyzer
-      pkgs.openssl
-      pkgs.nixVersions.nix_2_19
-      pkgs.nlohmann_json
-      pkgs.libsodium
-      pkgs.boost
-      pkgs.rustfmt
-
-    ];
-
-      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-  };
-}
+import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).defaultNix
--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
 
     (flake-utils.lib.eachDefaultSystem (system:
     let pkgs = nixpkgs.legacyPackages.${system}; in 

6389175889372de34bfeb785f042421eebdc8f1d
Julien Malka <julien@malka.sh>
2024-05-01 22:03:31 +0200

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -8,48 +8,63 @@
     queued-build-hook.url = "github:JulienMalka/queued-build-hook/postbuildscript";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, queued-build-hook, ... }:
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-utils,
+      queued-build-hook,
+      ...
+    }:
 
-    (flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = rec {
-          utils = pkgs.callPackage ./utils {};
-          default = utils;
-        };
+    (flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          packages = rec {
+            utils = pkgs.callPackage ./utils { };
+            default = utils;
+          };
 
+          checks.packages.utils = packages.utils;
 
-        checks.packages.utils = packages.utils;
-
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo gcc pkg-config ];
-          buildInputs = [
-            (pkgs.python3.withPackages (
-              ps: [
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              gcc
+              pkg-config
+            ];
+            buildInputs = [
+              (pkgs.python3.withPackages (ps: [
                 ps.fastapi
                 ps.uvicorn
                 ps.sqlalchemy
                 ps.pydantic
-              ]
-            ))
-            pkgs.jq
-            pkgs.rust-analyzer
-            pkgs.openssl
-            pkgs.nixVersions.nix_2_19
-            pkgs.nlohmann_json
-            pkgs.libsodium
-            pkgs.boost
-            pkgs.rustfmt
+              ]))
+              pkgs.jq
+              pkgs.rust-analyzer
+              pkgs.openssl
+              pkgs.nixVersions.nix_2_19
+              pkgs.nlohmann_json
+              pkgs.libsodium
+              pkgs.boost
+              pkgs.rustfmt
+            ];
 
-          ];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        }
+      )
+    )
+    // {
 
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-        };
-      })) // {
-
-        nixosModules.hash-collection = import ./utils/nixos/module.nix queued-build-hook.nixosModules.queued-build-hook;
-      };
-
-
+      nixosModules.hash-collection = import ./utils/nixos/module.nix queued-build-hook.nixosModules.queued-build-hook;
+    };
 }

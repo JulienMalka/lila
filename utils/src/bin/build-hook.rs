edb55e092ae77bdd5f6bf3bd20624540858d102c
Arnout Engelen <arnout@bzzt.net>
2025-02-20 13:37:19 +0100

    add getting nar hash and size

--- utils/src/bin/build-hook.rs
+++ utils/src/bin/build-hook.rs
@@ -1,10 +1,9 @@
-//use libnixstore::{query_path_info, sign_string, Radix::Base32};
 use nix_hash_collection_utils::*;
 use reqwest::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    //libnixstore::init();
+    let ctx = nix_init();
     let token = read_env_var_or_panic("HASH_COLLECTION_TOKEN");
     let secret_key = read_env_var_or_panic("HASH_COLLECTION_SECRET_KEY");
     let collection_server = read_env_var_or_panic("HASH_COLLECTION_SERVER");
@@ -20,9 +19,8 @@ async fn main() -> Result<()> {
     let output_attestations: Vec<_> = out_paths
         .split(" ")
         .map(|path| -> OutputAttestation {
-            //let info = query_path_info(path, Base32).unwrap();
-            //let hash = info.narhash;
-            //let size = info.size;
+            let hash = nar_hash(ctx, path.to_string());
+            let size = nar_size(ctx, path.to_string());
             //let fingerprint = fingerprint(path, &hash, size);
             //let signature = sign_string(secret_key.as_str(), &fingerprint).expect("Failed to sign fingerprint");
             let signature = my_sign_detached(secret_key.as_str(), String::from("42"));
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -3,15 +3,90 @@ use regex::Regex;
 use reqwest::Result;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::ptr::null;
+use std::ptr::null_mut;
 use libc::c_char;
 use std::ffi::{CStr,CString};
 
 // Should be moved to a standalone wrapper library such as
 // https://github.com/nixops4/nixops4/tree/main/rust/nix-util
+// when we do that we should also make other improvements such
+// as considering to use rust-bindgen etc, for now keep things
+// as simple as possible
+pub type err = ::std::os::raw::c_int;
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct c_context {
+    _unused: [u8; 0],
+}
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct Store {
+    _unused: [u8; 0],
+}
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct StorePath {
+    _unused: [u8; 0],
+}
+#[link(name = "nixstorec")]
+extern {
+    fn nix_c_context_create() -> *mut c_context;
+    fn nix_c_context_free() -> *mut c_context;
+
+    fn nix_libstore_init(context: *mut c_context) -> err;
+    fn nix_store_open(
+        context: *mut c_context,
+        uri: *const ::std::os::raw::c_char,
+        params: *mut *mut *const ::std::os::raw::c_char,
+    ) -> *mut Store;
+    fn nix_store_parse_path(
+        context: *mut c_context,
+        store: *mut Store,
+        path: *const c_char,
+    ) -> *mut StorePath;
+    fn nix_store_path_free(p: *mut StorePath);
+    fn nix_store_free(store: *mut Store);
+    fn nix_store_path_nar_hash(store: *mut Store, store_path: *const StorePath) -> *mut c_char;
+    fn nix_store_path_nar_size(store: *mut Store, store_path: *const StorePath) -> u64;
+}
 #[link(name = "nixutilc")]
 extern {
     fn hash_path(input: *const c_char) -> *mut c_char;
     fn sign_detached(secret_key: *const c_char, data: *const c_char) -> *mut c_char;
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Ctx {
+    context: *mut c_context,
+    store: *mut Store
+}
+pub fn nix_init() -> Ctx {
+    unsafe {
+        let ctx = nix_c_context_create();
+        nix_libstore_init(ctx);
+        let store = nix_store_open(ctx, null(), null_mut());
+        return Ctx { context: ctx, store: store };
+    }
+}
+pub fn nar_hash(ctx: Ctx, path: String) -> String {
+    unsafe {
+        let cpath = CString::new(path).unwrap();
+        let path = nix_store_parse_path(ctx.context, ctx.store, cpath.as_ptr());
+        let hash = CStr::from_ptr(nix_store_path_nar_hash(ctx.store, path));
+        let res = String::from_utf8_lossy(hash.to_bytes()).to_string();
+        nix_store_path_free(path);
+        return res;
+    }
+}
+pub fn nar_size(ctx: Ctx, path: String) -> u64 {
+    unsafe {
+        let cpath = CString::new(path).unwrap();
+        let path = nix_store_parse_path(ctx.context, ctx.store, cpath.as_ptr());
+        let res = nix_store_path_nar_size(ctx.store, path);
+        nix_store_path_free(path);
+        return res;
+    }
 }
 
 pub fn my_hash_path(input: String) -> String {

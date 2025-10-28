64b3352a73e8bef519ffcc5fab2ef9dca28926b8
Arnout Engelen <arnout@bzzt.net>
2025-10-28 18:50:12 +0100

    Pass derivation identifier to copy-from-cache if available
    Fixes #71
    
    Needs #79 to be deployed on the target server

--- fetch-from-cache.sh
+++ fetch-from-cache.sh
@@ -9,11 +9,18 @@ if [ "x" == "x$REPORT" ]; then
 fi
 
 while true; do
-  curl -H "Authorization: Bearer $HASH_COLLECTION_TOKEN" $HASH_COLLECTION_SERVER/reports/$REPORT/suggested | jq .[] | head -50 | tr -d \" | while read out
+  curl -H "Authorization: Bearer $HASH_COLLECTION_TOKEN" $HASH_COLLECTION_SERVER/reports/$REPORT/suggest | jq -c ".[] | pick(.out_path, .drv_path)" | head -50 | while read out
   do
-    echo $out
+    OUT_PATH=$(echo $out | cut -d '"' -f 4)
+    DRV_PATH=$(echo $out | cut -d '"' -f 8)
+    if [[ "x" == "x$DRV_PATH" ]]; then
+      DRV_PATH=$(nix derivation show $OUT_PATH | jq "keys.[]" | tr -d \")
+    fi
+    DRV_IDENT=$(echo $DRV_PATH | tail -c +12)
+    echo "Fetching $OUT_PATH from cache with deriver $DRV_PATH"
+
     # TODO some/most of these can probably also be taken found in the
     # local cache (with a cache.nixos.org signature), so perhaps take them from there?
-    copy-from-cache $out
+    copy-from-cache $OUT_PATH $DRV_IDENT
   done
 done
